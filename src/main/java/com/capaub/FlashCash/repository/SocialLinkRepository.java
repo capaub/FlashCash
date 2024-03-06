8216e94b9499e9ffe4e6c9b0f076a4fc1683660c
aubry <aubry@exemple.com>
2024-03-06 20:07:14 +0100

    add friendList DTO and HQL request

--- src/main/java/com/capaub/FlashCash/repository/SocialLinkRepository.java
+++ src/main/java/com/capaub/FlashCash/repository/SocialLinkRepository.java
@@ -1,9 +1,14 @@
 package com.capaub.FlashCash.repository;
 
 import com.capaub.FlashCash.entity.SocialLink;
+import com.capaub.FlashCash.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SocialLinkRepository extends JpaRepository<SocialLink, Integer> {
+    Optional<List<SocialLink>> findAllByUser1(User user);
 }
---
+++ src/main/java/com/capaub/FlashCash/service/form/FriendList.java
@@ -0,0 +1,9 @@
+package com.capaub.FlashCash.service.form;
+
+import lombok.Data;
+
+@Data
+public class FriendList {
+    private String friendName;
+    private String friendMail;
+}
