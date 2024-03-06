3d686327a1505244019b2605671688598b084fda
aubry <aubry@exemple.com>
2024-03-06 20:20:05 +0100

    add endPoint get with mail parameter and postMapping

--- src/main/java/com/capaub/FlashCash/controller/TransfersController.java
+++ src/main/java/com/capaub/FlashCash/controller/TransfersController.java
@@ -2,30 +2,26 @@ package com.capaub.FlashCash.controller;
 
 import com.capaub.FlashCash.entity.UserAccount;
 import com.capaub.FlashCash.service.SessionService;
+import com.capaub.FlashCash.service.TransferService;
 import com.capaub.FlashCash.service.UserAccountService;
-import com.capaub.FlashCash.service.UserService;
 import com.capaub.FlashCash.service.form.AddCashCbForm;
+import com.capaub.FlashCash.service.form.TransferForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TransfersController {
 
-    @Autowired
-    private final UserService userService;
-    @Autowired
+    private final TransferService transferService;
     private final SessionService sessionService;
-    @Autowired
     private final UserAccountService userAccountService;
 
-    public TransfersController(UserService userService, SessionService sessionService, UserAccountService userAccountService) {
-        this.userService = userService;
+    @Autowired
+    public TransfersController(TransferService transferService, SessionService sessionService, UserAccountService userAccountService) {
+        this.transferService = transferService;
         this.sessionService = sessionService;
         this.userAccountService = userAccountService;
     }
@@ -53,4 +49,22 @@ public class TransfersController {
         return new ModelAndView("/account", "user", sessionService.sessionUser());
     }
 
+    @GetMapping("/transferToFriend/{friendMail}")
+    @ResponseBody
+    public ModelAndView transferToAFriend(@PathVariable String friendMail, Model model) {
+        TransferForm transferForm = new TransferForm();
+        transferForm.setMailUserFrom(sessionService.sessionUser().getEmail());
+        transferForm.setMailUserTo(friendMail);
+
+        model.addAttribute("transferForm", transferForm);
+        model.addAttribute("maxAmount", sessionService.sessionUser().getUserAccount().getAmount());
+
+        return new ModelAndView("/transferForm");
+    }
+    @PostMapping("/transferToFriend")
+    public String processTransferToAFriend(@ModelAttribute("transferForm") TransferForm transferForm) {
+        transferService.transferCashToFriend(transferForm);
+
+        return "redirect:/showFriends";
+    }
 }
