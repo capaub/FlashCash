0cdb0c7f5fce2c37afea3a0edb62c184b9c959c1
aubry <aubry@exemple.com>
2024-03-06 20:11:39 +0100

    add showMyFriends()

--- src/main/java/com/capaub/FlashCash/service/UserAccountService.java
+++ src/main/java/com/capaub/FlashCash/service/UserAccountService.java
@@ -6,12 +6,12 @@ import com.capaub.FlashCash.entity.UserAccount;
 import com.capaub.FlashCash.repository.SocialLinkRepository;
 import com.capaub.FlashCash.repository.UserRepository;
 import com.capaub.FlashCash.service.form.AddCashCbForm;
+import com.capaub.FlashCash.service.form.FriendList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 public class UserAccountService {
@@ -58,20 +58,23 @@ public class UserAccountService {
 
     public void addAFriend(String userMail) {
         User userAddAFriend = sessionService.sessionUser();
-
         User friend = userRepository.findUserByEmail(userMail).get();
-
         SocialLink socialLink = new SocialLink();
-
         socialLink.setUser1(userAddAFriend);
         socialLink.setUser2(friend);
-
-        List<SocialLink> socialLinks = userAddAFriend.getSocialLinks();
         socialLinkRepository.save(socialLink);
+    }
 
-/*        userAddAFriend.setSocialLinks(socialLinks);
-
-        userRepository.save(userAddAFriend);*/
-
+    public List<FriendList> showMyFriends(User user) {
+        List<SocialLink> socialLinks = socialLinkRepository.findAllByUser1(user).get();
+        List<FriendList> friendsData = new ArrayList<>();
+        socialLinks.forEach(socialLink -> {
+            FriendList friendList = new FriendList();
+            User friend = socialLink.getUser2();
+            friendList.setFriendName(friend.getFirstName());
+            friendList.setFriendMail(friend.getEmail());
+            friendsData.add(friendList);
+        });
+        return friendsData;
     }
 }
